4a1b7d1d6712e971b4cd58951830d45258eed1eb
jaspersl <jaspersl@f127e5dc-2b97-4676-bf30-0bbaa7aa0b8e>
2009-09-18 20:49:14 +0000

    Default of config.inc.php is now FTP
    git-svn-id: svn://svn.code.sf.net/p/rcubevacation/code/trunk@27 f127e5dc-2b97-4676-bf30-0bbaa7aa0b8e

--- config.inc.php
+++ config.inc.php
@@ -50,7 +50,7 @@ $rcmail_config['setuid']['vacation_flags'] = ''; // See man vacation for valid f
 /*
 	FTP backend parameters
 */
-$rcmail_config['ftp']['server'] = 'ftp.xs4all.nl';
+$rcmail_config['ftp']['server'] = 'localhost';
 $rcmail_config['ftp']['passive'] = true;
 $rcmail_config['ftp']['vacation_executable'] = '/usr/bin/vacation';
 $rcmail_config['ftp']['vacation_flags'] = ''; // See man vacation for valid flags
